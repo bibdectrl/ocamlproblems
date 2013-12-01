9a9b717c7bdaee6e4ebda281b9df81d17e7d830b
bibdectrl <patrick.mooney@ryerson.ca>
2013-12-01 22:04:11 +0000

    done a couple more

--- lists.ml
+++ lists.ml
@@ -39,10 +39,27 @@ let length_tail_rec(list) =
 
 
 (* Reverse a list. *)
-(*not actually correct
+
 let rec reverse = function
   | [] -> []
-  | [x] -> [x]
-  | x::xs -> reverse (List.tl xs) @ [x]
-*)
+  | x::xs -> reverse xs @ [x]
 
+(* Find out whether a list is a palindrome *)
+
+let is_palindrome a b =
+  reverse a = b
+
+
+
+(* There is no nested list type in OCaml, so we need to define one first. A node of a nested list is either an element, or a list of nodes. *)
+
+type 'a node =
+| One of 'a 
+| Many of 'a node list;;
+
+type 'a node = One of 'a | Many of 'a node list
+
+(*
+let rec flatten = function
+  |
+*)
